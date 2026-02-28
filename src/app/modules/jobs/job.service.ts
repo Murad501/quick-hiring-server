441b72a50fe306c5735ffc2804eb4862ea46d193
Murad501 <murad.hossain5501@gmail.com>
2026-02-28 21:46:41 +0600

    feat: Implement transactional deletion of jobs and their associated applications.

--- src/app/modules/jobs/job.service.ts
+++ src/app/modules/jobs/job.service.ts
@@ -1,5 +1,7 @@
 import httpStatus from "http-status";
+import mongoose from "mongoose";
 import ApiError from "../../../errors/ApiError";
+import Application from "../applications/application.model";
 import { IJob } from "./job.interface";
 import Job from "./job.model";
 import { getUniqueKey } from "../../../helpers/getUniqueKey";
@@ -31,11 +33,25 @@ class JobService {
   }
 
   async deleteJob(jobId: string): Promise<IJob | null> {
-    const result = await this.job.findOneAndDelete({ jobId });
-    if (!result) {
-      throw new ApiError(httpStatus.NOT_FOUND, "Job not found");
+    const session = await mongoose.startSession();
+    try {
+      session.startTransaction();
+
+      const result = await this.job.findOneAndDelete({ jobId }, { session });
+      if (!result) {
+        throw new ApiError(httpStatus.NOT_FOUND, "Job not found");
+      }
+
+      await Application.deleteMany({ jobId }, { session });
+
+      await session.commitTransaction();
+      await session.endSession();
+      return result;
+    } catch (error) {
+      await session.abortTransaction();
+      await session.endSession();
+      throw error;
     }
-    return result;
   }
 }
 
